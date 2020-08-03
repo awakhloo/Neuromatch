3b42d041d91bf173e008f18ee5a2e95bbf090c30
awakhloo <65875866+awakhloo@users.noreply.github.com>
2020-08-02 22:22:47 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from glob import glob
+import torch
 
 def processer(folders, truncate = 1000, return_stim = True, mr_clean = True ) : 
     '''
@@ -182,7 +183,7 @@ def frequency_transform(folder, dat = None, truncate = 500, return_stim = True,
     return df, neur, stim_df 
 '''spike trains, labelled neurons with spike time stamps, stimulus df'''
 
-def pipeline(folders, path = None, truncate = 1000, bin_size = 0.02, mr_clean = True) : 
+def pipeline(folders, path = None, truncate = 1000, bin_size = 0.02, mr_clean = False) : 
     '''
     Args - 
     Path : file path for all Steinmetz mice folders for loading. 
@@ -287,4 +288,109 @@ def extract_trial_stim_avg(zone, stim, relevant_n, bins = .20) :
         mouse_df['maus'] = mouse 
         full_df = full_df.append(mouse_df, ignore_index = True)
     print('~~~ young success ~~~~')
-    return full_df 
+    return full_df
+
+def stim_filler(spk_df) : 
+    '''fills out stim times to cover the length of the trial. 
+    Args : spike_df, a frequency transform object obtained from the pipeline function
+    Returns : spike_df but with all of the stimulus times filled out, instead of just onset times
+    '''
+    response_t = spk_df[spk_df.response != 0].index
+    stim_onset = spk_df[spk_df.stim != 0].index
+    nearest_stim = np.zeros(len(response_t))
+
+    for i, time in enumerate(response_t) : 
+        '''grab indices of the closest stim == 1 entry for each response. necessary to reshape'''
+        nearest_stim[i] = stim_onset[stim_onset < time][-1]
+    nearest_stim = nearest_stim.astype(int)
+    spikes = spk_df.copy()
+    stim_fill = np.zeros(spikes.shape[0])
+    cr = spikes.contrast_right.values
+    cl = spikes.contrast_left.values  
+    
+    for onset, response in zip(nearest_stim, response_t) : 
+        stim_fill[onset:response + 1] = 1 #add one so that we still have stim type at t of reaction
+        cr[onset:response + 1] = cr[onset]
+        cl[onset:response + 1] = cl[onset]
+    
+    spikes['filled_stim'] = stim_fill
+    spikes['contrast_right'] = cr
+    spikes['contrast_left'] = cl
+    return spikes
+
+def nearest_stim(response_t_indices, stim_onset_indices) : 
+    '''Returns an array of indices the same size as response_t_indices containing the indices
+    of the closest time in a larger spike_df where stim was 1. This is a horrible sentence.
+    Basically this function takes a response time and tells you where in the data set was
+    the stimulus onset time. No need to call this function for any reason.'''
+    nearest_stim = np.zeros(len(response_t_indices))
+    for i, time in enumerate(response_t_indices) : 
+        '''grab indices of the closest stim == 1 entry for each response. necessary to reshape'''
+        nearest_stim[i] = stim_onset_indices[stim_onset_indices < time][-1]
+    return nearest_stim.astype(int)
+
+def trial_tensors(spks_df, chunk_behavior = False,
+                 chunk_spikes = False, get_rt = False, lunacy= False, bin_size = 0.02) : 
+    '''Form trial tuples for LSTM. This is a necessary step to form time series chunks that we
+    can then pass to pytorch.
+    Args : spks_df, a spike dataframe which has already been passed through the stimulus filler
+        chunk_behavior : whether to form the chunks out of behavioral data. The idea is that
+        well eventually be able to use this for the full  behavioral dat.
+        chunk_spikes : use this. chunks the spiking data into the trials.
+        get_rt : whether the outcome variable should contain the rt
+        bin_size : whatever bin_size you used when you called pipeline.
+        lunacy : introduce a halfway cut to the time series'''
+    trial_tups = []
+    #if lunacy == True : trial_tups = [[], []]
+    resp = spks_df[spks_df.response != 0].index
+    nearest_st = nearest_stim(resp, 
+                             spks_df[spks_df.stim != 0].index) # get chunk indices
+    for start, end in zip(nearest_st, resp) : 
+        trl_slice = spks_df.iloc[start:end + 1]
+        if trl_slice.iloc[-1].response == 999 : continue #don't grab no gos
+            
+        if chunk_behavior == True : 
+            #eventually we'll be grabbing more than just the contrasts. In reality we don't
+            #even need this function, as it'd be way more efficient to do this without chunking
+            stim_trl_slice = trl_slice.loc[:, ['contrast_left', 'contrast_right']].values.astype(float)
+            if get_rt == True : #make the outcome tensor out of rt + choice
+                behavioral_re = np.array([float(stim_trl_slice.shape[0]) * bin_size,
+                                          trl_slice.loc[:, 'response'].values[-1]])
+
+            else : 
+                behavioral_re = trl_slice.loc[:, 'response'].values.astype(float)
+                #print(behavioral_re[-1])
+                
+            stim_trl_slice = torch.tensor(stim_trl_slice, requires_grad = True).float()
+            behavioral_re = torch.tensor(behavioral_re, requires_grad = True).float()
+            if lunacy == True : 
+                trial_tups[0].append(stim_trl_slice) 
+                trial_tups[1].append(behavioral_re)
+            else :
+                trial_tups.append((stim_trl_slice, behavioral_re))
+            
+        elif chunk_spikes == True : 
+            #grab the neural chunks booooiIIIIIII
+            neur_trl_slice = trl_slice.iloc[:, :-8].values.astype(float) 
+            
+            if get_rt == True : 
+                behavioral_re = np.array([float(neur_trl_slice.shape[0]) * bin_size,
+                                          trl_slice.loc[:, 'response'].values[-1]])
+            else : 
+                behavioral_re = trl_slice.loc[:, 'response'].values.astype(float)
+            neur_trl_slice = torch.tensor(neur_trl_slice, requires_grad = True).float()
+            behavioral_re = torch.tensor(behavioral_re, requires_grad = True).float()
+            
+            if lunacy == True : 
+                cut_n = int(neur_trl_slice.shape[0] / 2) 
+                
+                trial_tups.append((neur_trl_slice[:cut_n], torch.reshape(behavioral_re[cut_n], (1, ))))
+                trial_tups.append((neur_trl_slice[cut_n:], torch.reshape(behavioral_re[-1], (1, ))))
+
+            else : 
+                trial_tups.append((neur_trl_slice, torch.reshape(behavioral_re[-1], (1,))))
+            
+    return trial_tups
+
+
+
